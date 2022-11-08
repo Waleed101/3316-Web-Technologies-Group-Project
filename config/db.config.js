3651dc0774b2f9b7520aed1a7eaa68f8bca88513
Waleed Sawan <waleed.sawan@gmail.com>
2022-11-08 14:59:00 -0500

    Need to add passwords to make it work on the AWS

---
+++ config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    HOST: "database-1.cjrkveiinmqa.us-east-1.rds.amazonaws.com",
+    PORT: "3306",
+    USER: "admin",
+    PASSWORD: "Waleed123-",
+    DB: "my_db"
+  };
+
+console.log("Got Database Passwords")
