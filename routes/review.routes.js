06d786bc995bb9194e3d7f7e74d942510fc6c240
Waleed Sawan <waleed.sawan@gmail.com>
2022-11-13 17:35:22 -0500

    Creating review controller, model, and routes

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -12,8 +12,8 @@ module.exports = app => {
     // Retrieve a single review with id
     router.get("/:id", list.findOne);
   
-    // Update a Review with name
-    router.put("/:name", list.update);
+    // Update a Review with id
+    router.put("/:id", list.update);
   
     // Delete a Review with name
     router.delete("/:id", list.review);
